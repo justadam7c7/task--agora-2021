54f014e28461ce7e05f0eaba21e06759b66ad7d7
justadam7c7 <jusada7c7@gmail.com>
2021-06-01 14:23:43 +0200

    removing unnecessary comments

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var testObject = (function () {
     const _myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     function init() {
-        // Tu niczego nie zmieniaj
         return _changeArrayStructure(_checkArrayValues());
     }
 
@@ -25,22 +24,13 @@ var testObject = (function () {
             newDiv.className = 'clicked';
         });
         return document;
-        // Ta funkcja bedzie zwracała dynamicznie stworzony element DIV.
-        // Będzie on wewnątrz zawierał elementy SPAN oraz BUTTON.
-        // Po kliknięciu w BUTTON, dla elementu DIV będzie ustawiana klasa 'clicked'.
-        // Element SPAN będzie zawierał tekst `Użytkownik YOUR_NAME` - gdzie YOUR_NAME to Twoje imię i nazwisko.
-        // Twoje imię i nazwisko będzie przekazywane do tej funkcji jako argument 'user'.
-        // Do elementu DIV będzie dodawana klasa 'userTest'.
-        // Na koniec funkcja zwróci gotową strukturę elementów HTML, opisaną w powyższych krokach.
     }
 
     function api() {
-        // Tu niczego nie zmieniaj - funkcja będzie przydatna do testów jednostkowych
         return _simulateApiResponse().then(res => res);
     }
 
     async function _simulateApiResponse() {
-        // Tu niczego nie zmieniaj - funkcja będzie przydatna do testów jednostkowych
         return await new Promise((resolve) => setTimeout(resolve, 100, {status: 200}));
     }
 
@@ -48,15 +38,12 @@ var testObject = (function () {
         return _myArray.filter((elem) => {
             return elem > 4;
         });
-        // Funkcja zwraca tylko te elementy tablicy _myArray, które są większe od 4.
     }
 
     function _changeArrayStructure(arr) {
         return arr.map(x => {
             return {item: x};
         });
-        // Funkcja przyjmuje tablicę cyfr jako argument i zwraca jej zmienioną strukturę.
-        // Struktura ma wyglądać następująco: { item: INDEKS_1, item: INDEKS_2, itd. }
     }
 
     return {
@@ -65,7 +52,6 @@ var testObject = (function () {
         api: api
     }
 }());
-// kod sprawdzający wyniki zwracane przez funkcje
 console.log(
     testObject.init(),
     testObject.createHTML('Justyna Adamczyk')
--- main.test.js
+++ main.test.js
@@ -14,38 +14,28 @@ describe('testObject test', () => {
     });
 
     test('createHTML() return proper HTML structure', () => {
-        // sprawdź czy funkcja createHTML() zwraca element DIV
-        // i czy zawiera on elementy SPAN i BUTTON
-        const div = element.querySelector('div');
+               const div = element.querySelector('div');
         expect(div).toBeTruthy();
         expect(div.querySelector('span')).toBeTruthy();
         expect(div.querySelector('button')).toBeTruthy();
     });
 
     test('DIV element has set proper class', () => {
-        // sprawdź czy element DIV ma ustawioną klasę 'userTest'
-        const div = element.querySelector('.userTest');
+                const div = element.querySelector('.userTest');
         expect(div).toBeTruthy();
     });
 
     test('SPAN element has set proper text', () => {
-        // sprawdź czy element SPAN zawiera tekst
-        // `Użytkownik: YOUR_NAME` - gdzie YOUR_NAME to Twoje imię i nazwisko
-        expect(span.textContent).toContain('Użytkownik Justyna Adamczyk');
+                expect(span.textContent).toContain('Użytkownik Justyna Adamczyk');
     });
 
     test('BUTTON click working properly', () => {
-        // sprawdź czy po kliknięciu w BUTTON
-        // ustawiana jest klasa 'clicked' na elemencie DIV
-        btn.click();
+               btn.click();
         const div = element.querySelector('.clicked');
         expect(div).toBeTruthy();
     });
 
     test('api() returns expected data', () => {
         return expect(main.api()).resolves.toHaveProperty('status', 200);
-        // DLA CHĘTNYCH
-        // Sprawdź czy funkcja api() zwraca oczekiwane dane: {status: 200}
-        // Możesz włączyć test, zmieniając słowo 'xtest' na 'test'
-    });
+           });
 });
